f9435707877a5cf4b02c5842f5cbee12d3536477
Leandro Coelho e Silva <lndsystem@users.noreply.github.com>
2023-09-18 13:27:55 +0000

    ajustando o endpoint

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,3 +1,7 @@
 function status(request, response) {
-  response.status(200).send("alunos do curso.dev são pessoas acima da média.");
+  response
+    .status(200)
+    .json({ chave: "alunos do curso.dev são pessoas acima da média." });
 }
+
+export default status;
