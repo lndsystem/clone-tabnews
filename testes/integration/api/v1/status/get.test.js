acfdaa5d8476f3e5b2687bf9736bfb4116b79cb7
Leandro Coelho e Silva <leandroces@gmail.com>
2023-10-24 05:49:37 -0300

    criando conexão ao banco

--- testes/integration/api/v1/status/get.test.js
+++ testes/integration/api/v1/status/get.test.js
@@ -1,4 +1,6 @@
+const fetch = require("node-fetch");
+
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch("http://localhost:3000/api/v1/status");  
   expect(response.status).toBe(200);
 });
